4f4d564bd5c0c934908bb16fc1c13ce270f52390
Sandro Augusto Fonseca Junqueira <sandro.junqueira@verreschi.com>
2019-08-15 16:05:59 -0300

    Version 0.1

--- Gerson.py
+++ Gerson.py
@@ -1,3 +1,5 @@
+from time import sleep 
+
 prato1 = str('Prato 1: Arroz com Bife  R$:17,00')
 prato2 = str('Prato 2: Macarrão bolonhesa  R$:12,00')
 prato3 = str('Prato 3: Nhoque bolonhesa  R$:13,00')
@@ -6,6 +8,15 @@ prato5 = str('Prato 5: Escondidinho de Carne  R$:11,00')
 prato6 = str('Prato 6: Escondidinho de Frango  R$:10,00')
 prato7 = str('Prato 7: Sopa de Ervilha  R$:8,00')
 prato8 = str('Prato 8: Salada do Chefe  R$:5,00')
+
+bebida1 = str('Bebida 1: Água sem gás  R$:4,00')
+bebida2 = str('Bebida 2: Água com gás  R$:5,00')
+bebida3 = str('Bebida 3: Refrigerante de Lata  R$:6,00')
+bebida4 = str('Bebida 4: Suco Natural  R$:10,00')
+bebida5 = str('Bebida 5: Cerveja de Lata  R$:7,00')
+bebida6 = str('Bebida 6: Vinho  R$:12,00')
+bebida7 = str('Bebida 7: Saquê  R$:20,00')
+bebida8 = str('Bebida 8: Energético  R$:7,99')
 
 print('\nOlá, bem vindo ao nosso restaurante. Eu me chamo Gerson')
 
@@ -22,8 +33,12 @@ if pergunta1.upper() == 'NÃO':
     cardapio = str(input('\nGostaria então de ver nosso cardapio? '))
     if cardapio.upper() == 'SIM':
         print('\nAqui está nosso cardapio')
-        print( '\n',prato1, '\n',prato2, '\n',prato3, '\n',prato4,
+        print('\nPratos:')
+        print('\n',prato1, '\n',prato2, '\n',prato3, '\n',prato4,
          '\n',prato5, '\n',prato6, '\n',prato7, '\n',prato8)
+        print('\nBebidas:')
+        print('\n',bebida1, '\n',bebida2, '\n',bebida3, '\n',bebida4,
+         '\n',bebida5, '\n',bebida6, '\n',bebida7, '\n',bebida8,)
     
     pergunta2 = str(input('\nJá se decidiu? '))
     if pergunta2.upper() == 'SIM':
@@ -37,7 +52,7 @@ if pergunta1.upper() == 'NÃO':
             elif pedido2.upper() == 'VOU QUERER O PRATO 3':
                 print('\nCerto, vou prepara-lo imediadamente!')
             elif pedido2.upper() == 'VOU QUERER O PRATO 4':
-                print('\nAmante de Strogoff né? Vou mandar prepar!')
+                print('\nAmante de Strogoff né? Vou mandar preparar!')
             elif pedido2.upper() == 'VOU QUERER O PRATO 5':
                 print('\nPerfeito, já está fazendo!')
             elif pedido2.upper() == 'VOU QUERER O PRATO 6':
@@ -45,7 +60,32 @@ if pergunta1.upper() == 'NÃO':
             elif pedido2.upper() == 'VOU QUERER O PRATO 7':
                 print('\nUma sopa saindo!')
             elif pedido2.upper() == 'VOU QUERER O PRATO 8':
-                print('\nOk, a iguraria do Chefe então!')
+                print('\nOk, a iguaria do Chefe então!')
+            
+            pedirbebida = str(input('\nAceita uma bebida? '))
+            if pedirbebida.upper() == 'SIM':
+                print('\nQual seria?')
+                resulbebida = str(input(''))
+                if resulbebida.upper() == 'VOU QUERER A BEBIDA 1':
+                    print('Boa escolha, uma opção sempre saudavel!')
+                elif resulbebida.upper() == 'VOU QUERER A BEBIDA 2':
+                    print('Um gás saudavel, tudo bem')
+                elif resulbebida.upper() == 'VOU QUERER A BEBIDA 3':
+                    print('Um refrigerante, tudo bem')
+                elif resulbebida.upper() == 'VOU QUERER A BEBIDA 4':
+                    print('Suco natural é sempre bom mesmo!')
+                elif resulbebida.upper() == 'VOU QUERER A BEBIDA 5':
+                    print('Uma cervejinha gelada saindo!')
+                elif resulbebida.upper() == 'VOU QUERER A BEBIDA 6':
+                    print('Um vinho pra aquecer a noite')
+                elif resulbebida.upper() == 'VOU QUERER A BEBIDA 7':
+                    print('Animar a noite!')
+                elif resulbebida.upper() == 'VOU QUERER A BEBIDA 8':
+                    print('Otimo pra ter uma noite agitada!')
+            elif pedirbebida.upper() == 'NÃO':
+                print('\nTudo bem, vou mandar fazer seu pedido')
+            
+
             
     elif pergunta2.upper() == 'NÃO':
         print('\nOk, assim que se decidir pode me chamar!')
@@ -61,7 +101,7 @@ if pergunta1.upper() == 'NÃO':
             elif pedido7.upper() == 'VOU QUERER O PRATO 3':
                 print('\nCerto, vou prepara-lo imediadamente!')
             elif pedido7.upper() == 'VOU QUERER O PRATO 4':
-                print('\nAmante de Strogoff né? Vou mandar prepar!')
+                print('\nAmante de Strogoff né? Vou mandar preparar!')
             elif pedido7.upper() == 'VOU QUERER O PRATO 5':
                 print('\nPerfeito, já está fazendo!')
             elif pedido7.upper() == 'VOU QUERER O PRATO 6':
@@ -69,7 +109,30 @@ if pergunta1.upper() == 'NÃO':
             elif pedido7.upper() == 'VOU QUERER O PRATO 7':
                 print('\nUma sopa saindo!')
             elif pedido7.upper() == 'VOU QUERER O PRATO 8':
-                print('\nOk, a iguraria do Chefe então!')
+                print('\nOk, a iguaria do Chefe então!')
+
+            pedirbebida1 = str(input('\nAceita uma bebida? '))
+            if pedirbebida1.upper() == 'SIM':
+                print('\nQual seria?')
+                resulbebida1 = str(input('\n'))
+                if resulbebida1.upper() == 'VOU QUERER A BEBIDA 1':
+                    print('\nBoa escolha, uma opção sempre saudavel!')
+                elif resulbebida1.upper() == 'VOU QUERER A BEBIDA 2':
+                    print('\nUm gás saudavel, tudo bem')
+                elif resulbebida1.upper() == 'VOU QUERER A BEBIDA 3':
+                    print('\nUm refrigerante, tudo bem')
+                elif resulbebida1.upper() == 'VOU QUERER A BEBIDA 4':
+                    print('\nSuco natural é sempre bom mesmo!')
+                elif resulbebida1.upper() == 'VOU QUERER A BEBIDA 5':
+                    print('\nUma cervejinha gelada saindo!')
+                elif resulbebida1.upper() == 'VOU QUERER A BEBIDA 6':
+                    print('\nUm vinho pra aquecer a noite')
+                elif resulbebida1.upper() == 'VOU QUERER A BEBIDA 7':
+                    print('\nAnimar a noite!')
+                elif resulbebida1.upper() == 'VOU QUERER A BEBIDA 8':
+                    print('\nOtimo pra ter uma noite agitada!')
+            elif pedirbebida1.upper() == 'NÃO':
+                print('\nTudo bem, vou mandar fazer seu pedido')
 
         
 
@@ -84,7 +147,7 @@ elif pergunta1.upper() == 'SIM':
         elif pedido3.upper() == 'VOU QUERER O PRATO 3':
             print('\nCerto, vou prepara-lo imediadamente!')
         elif pedido3.upper() == 'VOU QUERER O PRATO 4':
-            print('\nAmante de Strogoff né? Vou mandar prepar!')
+            print('\nAmante de Strogoff né? Vou mandar preparar!')
         elif pedido3.upper() == 'VOU QUERER O PRATO 5':
             print('\nPerfeito, já está fazendo!')
         elif pedido3.upper() == 'VOU QUERER O PRATO 6':
@@ -92,7 +155,29 @@ elif pergunta1.upper() == 'SIM':
         elif pedido3.upper() == 'VOU QUERER O PRATO 7':
             print('\nUma sopa saindo!')
         elif pedido3.upper() == 'VOU QUERER O PRATO 8':
-            print('\nOk, a iguraria do Chefe então!')
+            print('\nOk, a iguaria do Chefe então!')
+        pedirbebida2 = str(input('\nAceita uma bebida? '))
+        if pedirbebida2.upper() == 'SIM':
+            print('\nQual seria?')
+            resulbebida2 = str(input('\n'))
+            if resulbebida2.upper() == 'VOU QUERER A BEBIDA 1':
+                print('\nBoa escolha, uma opção sempre saudavel!')
+            elif resulbebida2.upper() == 'VOU QUERER A BEBIDA 2':
+                print('\nUm gás saudavel, tudo bem')
+            elif resulbebida2.upper() == 'VOU QUERER A BEBIDA 3':
+                print('\nUm refrigerante, tudo bem')
+            elif resulbebida2.upper() == 'VOU QUERER A BEBIDA 4':
+                print('\nSuco natural é sempre bom mesmo!')
+            elif resulbebida2.upper() == 'VOU QUERER A BEBIDA 5':
+                print('\nUma cervejinha gelada saindo!')
+            elif resulbebida2.upper() == 'VOU QUERER A BEBIDA 6':
+                print('\nUm vinho pra aquecer a noite')
+            elif resulbebida2.upper() == 'VOU QUERER A BEBIDA 7':
+                print('\nAnimar a noite!')
+            elif resulbebida2.upper() == 'VOU QUERER A BEBIDA 8':
+                print('\nOtimo pra ter uma noite agitada!')
+        elif pedirbebida2.upper() == 'NÃO':
+            print('\nTudo bem, vou mandar fazer seu pedido')
             
     elif pedido1.upper() == 'AINDA NÃO':
         print('\nOk, retornarei mais tarde')
@@ -109,7 +194,7 @@ elif pergunta1.upper() == 'SIM':
             elif pedido5.upper() == 'VOU QUERER O PRATO 3':
                 print('\nCerto, vou prepara-lo imediadamente!')
             elif pedido5.upper() == 'VOU QUERER O PRATO 4':
-                print('\nAmante de Strogoff né? Vou mandar prepar!')
+                print('\nAmante de Strogoff né? Vou mandar preparar!')
             elif pedido5.upper() == 'VOU QUERER O PRATO 5':
                 print('\nPerfeito, já está fazendo!')
             elif pedido5.upper() == 'VOU QUERER O PRATO 6':
@@ -117,7 +202,30 @@ elif pergunta1.upper() == 'SIM':
             elif pedido5.upper() == 'VOU QUERER O PRATO 7':
                 print('\nUma sopa saindo!')
             elif pedido5.upper() == 'VOU QUERER O PRATO 8':
-                print('\nOk, a iguraria do Chefe então!')
+                print('\nOk, a iguaria do Chefe então!')
+
+            pedirbebida3 = str(input('\nAceita uma bebida? '))
+            if pedirbebida3.upper() == 'SIM':
+                print('\nQual seria?')
+                resulbebida3 = str(input('\n'))
+                if resulbebida3.upper() == 'VOU QUERER A BEBIDA 1':
+                    print('\nBoa escolha, uma opção sempre saudavel!')
+                elif resulbebida3.upper() == 'VOU QUERER A BEBIDA 2':
+                    print('\nUm gás saudavel, tudo bem')
+                elif resulbebida3.upper() == 'VOU QUERER A BEBIDA 3':
+                    print('\nUm refrigerante, tudo bem')
+                elif resulbebida3.upper() == 'VOU QUERER A BEBIDA 4':
+                    print('\nSuco natural é sempre bom mesmo!')
+                elif resulbebida3.upper() == 'VOU QUERER A BEBIDA 5':
+                    print('\nUma cervejinha gelada saindo!')
+                elif resulbebida3.upper() == 'VOU QUERER A BEBIDA 6':
+                    print('\nUm vinho pra aquecer a noite')
+                elif resulbebida3.upper() == 'VOU QUERER A BEBIDA 7':
+                    print('\nAnimar a noite!')
+                elif resulbebida3.upper() == 'VOU QUERER A BEBIDA 8':
+                    print('\nOtimo pra ter uma noite agitada!')
+            elif pedirbebida3.upper() == 'NÃO':
+                print('\nTudo bem, vou mandar fazer seu pedido')
 
 from time import sleep 
 print('\nPreparando pedido')
@@ -135,6 +243,8 @@ sleep(2)
 print('Finalizando pedido..')
 sleep(2)
 print('Finalizando pedido...')
-sleep(4)
+sleep(6)
 
 print('\nSeu pedido está pronto, tenha um bom apetite!')
+
+sleep(5)
